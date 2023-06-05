7f9f838074d42505e26eecf82293e381c75966f1
nIBOP <dubrovinor@gmail.com>
2023-06-05 13:32:52 +0300

    start done

--- src/Start.jsx
+++ src/Start.jsx
@@ -1,27 +1,33 @@
 import './Start.scss'
+import hat from './res/hat.svg'
 
 function Start() {
     return(
         <div className="Start">
-            <div className="dialog-1">
-                Привет, ты оказался на нашем  сайте для сбора друзей и 
-                <br />
-                организации хорошего время препровождения.
+            <div className="dialog">
+                <div className="dialog-1">
+                    <p>Привет, ты оказался на нашем  сайте для сбора друзей и 
+                    <br />
+                    организации хорошего время препровождения.</p>
+                </div>
+                <div className="dialog-2">
+                    <p>Давай познакомимся с  базовыми
+                    <br />
+                    функциями нашего сайта.</p>
+                </div>
+                <div className="dialog-3">
+                    <p>Если ты хочешь сразу приступить к созданию мероприятия, <a href="/events">жми сюда.</a></p> 
+                </div>
+                <div className="dialog-4">
+                    <p>Если хочешь перейти на страничку мероприятия, <a href="/search">жми сюда.</a></p> 
+                </div>
             </div>
-            <div className="dialog-2">
-                Давай познакомимся с  базовыми
-                <br />
-                функциями нашего сайта.
+            <div className="but-container">
+                <button className='but-start' >
+                    <img id='hat' src={hat} alt="" />
+                    НАЧАТЬ
+                </button>
             </div>
-            <div className="dialog-3">
-                Если ты хочешь сразу приступить к созданию мероприятия, жми сюда.
-            </div>
-            <div className="dialog-4">
-                Если хочешь перейти на страничку мероприятия, жми сюда.
-            </div>
-            <button className='but-start' >
-                НАЧАТЬ
-            </button>
         </div>
     );
 }
